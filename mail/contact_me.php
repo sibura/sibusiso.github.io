551487c00f5686db253ae092e6b00bdcb9a8f8f8
sibusiso <sibusiso@projectcodex.co>
2017-06-12 12:44:19 +0200

    modify

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,46 @@
 <?php
+require_once('../mail/class.phpmailer.php');
+
+$emailaddress = "name@domain.com"; 
+
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
+if(empty($_POST['name'])        ||
+   empty($_POST['email'])       ||
+   empty($_POST['phone'])       ||
+   empty($_POST['message'])     ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
-	return false;
+    echo "No arguments Provided!";
+    return false;
    }
-	
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-	
-// Create the email and send the message
-$to = 'sbuja.gbza@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-return true;			
+
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$phone = $_POST['phone'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$message = $_POST['message'];
+
+
+$subject = "Contact Form: $name";
+$body = "You have received a new message from website.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nIP Address: $ip\n\nMessage:\n$message";
+
+$mail             = new PHPMailer();
+$mail->IsSMTP(); 
+$mail->Host       = "smtp.mandrillapp.com"; 
+//$mail->SMTPDebug  = 1;                     
+$mail->SMTPAuth   = true;                 
+$mail->Host       = "smtp.mandrillapp.com"; 
+$mail->Port       = 587;                    
+$mail->Username   = "username"; 
+$mail->Password   = "password";        
+
+$mail->CharSet  = 'UTF-8';  
+$mail->SetFrom($email_address);
+$mail->AddReplyTo($email_address);
+$mail->Subject    = "Contact Form: ".$_POST['name']." ";
+$mail->MsgHTML($body);
+
+$mail->AddAddress($emailaddress);
+$mail->Send();
+
 ?>
