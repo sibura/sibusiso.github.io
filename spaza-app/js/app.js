e3253dc4381c7cabd02643be8db599b9ddd6b26e
sibusiso <sibusiso@projectcodex.co>
2017-06-19 10:11:49 +0200

    spaza-project modify

--- spaza-app/js/app.js
+++ spaza-app/js/app.js
@@ -8,16 +8,16 @@ myApp.controller("myController", function($scope){
 
 
     $scope.users= [
-        {username:"Sbu", email:"sbuja@gmail.com", fullname:"sibusiso"},
-        {username:"Sbuja", email:"sbu@gmail.com", fullname:"sibu"},
-        {username:"Sbura", email:"sbur@gmail.com", fullname:"sibu"},
-         {username:"Sbu", email:"sbuja@gmail.com", fullname:"sibusiso"},
-        {username:"Sbuja", email:"sbu@gmail.com", fullname:"sibu"},
-        {username:"Sbura", email:"sbur@gmail.com", fullname:"sibu"},
-         {username:"Sbu", email:"sbuja@gmail.com", fullname:"sibusiso"},
-        {username:"Sbuja", email:"sbu@gmail.com", fullname:"sibu"},
-        {username:"Sbura", email:"sbur@gmail.com", fullname:"sibu"},
-        {username:"Sbura", email:"sbur@gmail.com", fullname:"sibu"}
+        {product_name:"Milk ", category_name:"Dairy Products", fullname:"sibusiso"},
+        {product_name:"Bread", category_name:"Bakery Product", fullname:"sibu"},
+        {product_name:"Chakalaka Can", category_name:"Can Food", fullname:"sibu"},
+         {product_name:"Iwisa Pap 5kg", category_name:"Bulk", fullname:"sibusiso"},
+        {product_name:"Fanta 500ml", category_name:"cold Beverages", fullname:"sibu"},
+        {product_name:"Shampoo", category_name:"cosmetics", fullname:"sibu"},
+        {product_name:"Bananas", category_name:"fruits", fullname:"sibusiso"},
+        {product_name:"Mixed Sweets 5s", category_name:"Confectionaries", fullname:"sibu"},
+        {product_name:"Soap Bar", category_name:"Soup", fullname:"sibu"},
+        // {product_name:"Sbura", category_name:"sbur@gmail.com", fullname:"sibu"}
     ];
 
     $scope.saveUser = function(){
